c90c5eaecb9ae07271767df1d5d8289185f83e3d
Daniel Hauschildt <daniel.hauschildt@9elements.com>
2017-08-18 15:02:07 +0200

    fix: Some issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 # config valid only for current version of Capistrano
 
-set :application, 'dashboard'
-set :repo_url, 'ssh://git@github.com:imgly/#{application}.git'
+set :application, 'pesdk-internal-dashboard'
+set :repo_url, 'git@github.com:imgly/#{application}.git'
+#set :repo_url, 'ssh://git@github.com:imgly/#{application}.git'
 set :ssh_options, { forward_agent: true }
 set :linked_dirs, %w{log tmp}
--- jobs/chartmogul.rb
+++ jobs/chartmogul.rb
@@ -3,26 +3,41 @@ require 'chartmogul'
 ChartMogul.account_token = 'cb67558394369d9edb40105f88826e9b'
 ChartMogul.secret_key = 'bf324aa567cade946a5843c097c70fb2'
 
+transform = {
+  'arr': Proc.new { |val| val/100 },
+  'mrr':  Proc.new { |val| val/100 },
+  'asp':  Proc.new { |val| val/100 },
+  'customers':  Proc.new { |val| val }
+}
+
 SCHEDULER.every '200s', first_in: '0s' do
   metrics = ChartMogul::Metrics.all(start_date: '2017-07-01', end_date: '2017-08-17', interval: 'month')
 
   previous_metrics =  metrics.entries.first
   current_metrics =  metrics.entries.last
 
-  ['arr', 'mrr'].each do |key|
-    current = current_metrics.send(key)
-    previous = previous_metrics.send(key)
-    data_id = ['chartmogul', key].join(':')
-    send_event(data_id, { current: current, last: previous })
-  end
-
-  # require 'pry'
-  # binding.pry
-  send_event('chartmogul:mrr_goal', {
-    current: current_metrics.mrr/50000,
-    last: previous_metrics.mrr/50000
+  send_event(['chartmogul', 'mrr'].join(':'), {
+    current: current_metrics.mrr/100,
+    last: previous_metrics.mrr/100,
   })
 
+  send_event(['chartmogul', 'arr'].join(':'), {
+    current: current_metrics.arr/100,
+    last: previous_metrics.arr/100,
+  })
 
+  send_event(['chartmogul', 'asp'].join(':'), {
+    current: current_metrics.asp/100,
+    last: previous_metrics.asp/100,
+  })
+
+  send_event(['chartmogul', 'customers'].join(':'), {
+    current: current_metrics.customers,
+    last: previous_metrics.customers,
+  })
+
+  send_event( ['chartmogul', 'mrr_goal'].join(':'), {
+    value: current_metrics.mrr/50000
+  })
 
 end
