d9ed889de547de8e94af0650e63b88a0b0a2d86b
Daniel Hauschildt <daniel.hauschildt@9elements.com>
2017-08-18 14:23:46 +0200

    fix: deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid only for current version of Capistrano
 
-set :application, 'imgly internal dashboard'
-set :repo_url, 'ssh://git@github.com:imgly/dashboard.git'
+set :application, 'dashboard'
+set :repo_url, 'ssh://git@github.com:imgly/#{application}.git'
 set :ssh_options, { forward_agent: true }
 set :linked_dirs, %w{log tmp}
