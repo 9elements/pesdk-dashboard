0da0381a4a2af4771cf08a874065accbb7bdbced
Daniel Hauschildt <daniel.hauschildt@9elements.com>
2017-08-18 15:03:43 +0200

    fix: deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,6 @@
 # config valid only for current version of Capistrano
 
 set :application, 'pesdk-internal-dashboard'
-set :repo_url, 'git@github.com:imgly/#{application}.git'
-#set :repo_url, 'ssh://git@github.com:imgly/#{application}.git'
+set :repo_url, "git@github.com:imgly/#{fetch(:application)}.git"
 set :ssh_options, { forward_agent: true }
 set :linked_dirs, %w{log tmp}
