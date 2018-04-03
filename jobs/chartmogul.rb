d1fed9ff7029f48ac138c6bb77a8f72010743dc1
Marvin Koch <marvin.koch@9elements.com>
2018-04-03 15:57:56 +0200

    changed goals to 84 and 100k

--- jobs/chartmogul.rb
+++ jobs/chartmogul.rb
@@ -3,8 +3,8 @@ require 'pry'
 ChartMogul.account_token = 'cb67558394369d9edb40105f88826e9b'
 ChartMogul.secret_key = 'bf324aa567cade946a5843c097c70fb2'
 
-
-next_mrr_goal = 84000
+intermedia_mrr_goal = 8400000
+next_mrr_goal = 10000000
 
 SCHEDULER.every '200s', first_in: '0s' do
   end_date = DateTime.now.iso8601.slice(0,10)
@@ -77,10 +77,10 @@ SCHEDULER.every '200s', first_in: '0s' do
   })
 
   send_event( ['chartmogul', 'intermedia_mrr_goal'].join(':'), {
-    value: ((100 / 7000000.to_f) * current_metrics.mrr).to_i
+    value: ((100 / intermedia_mrr_goal.to_f) * current_metrics.mrr).to_i
   })
 
   send_event( ['chartmogul', 'next_mrr_goal'].join(':'), {
-    value: ((100 / (next_mrr_goal*100).to_f) * current_metrics.mrr).to_i
+    value: ((100 / next_mrr_goal.to_f) * current_metrics.mrr).to_i
   })
 end
